180d1437ee6ca56d6f447a7c741829483693238e
Logan <loganx@byu.edu>
2025-02-26 19:53:05 -0700

    Implemented the UserService class.
    The UserService class also required the Result and Request classes. So I created records to hold that information.

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -1,13 +1,39 @@
 package service;
 
+import dataaccess.AuthDao;
+import dataaccess.UserDao;
 import request.LoginRequest;
 import request.LogoutRequest;
 import request.RegisterRequest;
 import response.LoginResult;
+import response.RegisterResult;
+import response.LogoutResult;
 
 public class UserService {
-    public RegisterResult register(RegisterRequest registerRequest){}
-    public LoginResult login(LoginRequest loginRequest){}
-    public LogoutResult register(LogoutRequest logoutRequest){}
+
+    private final UserDao userDao;
+    private final AuthDao authDao;
+
+    public UserService(UserDao userDao, AuthDao authDao) {
+        this.userDao = userDao;
+        this.authDao = authDao;
+    }
+    public RegisterResult register(RegisterRequest registerRequest){
+        String user = registerRequest.username();
+        String password = registerRequest.password();
+        String email = registerRequest.email();
+        if(userDao.getUser(user) == null){
+            userDao.createUser(user, password, email);
+            authDao.createAuth(user);
+        }else{
+
+        }
+    }
+    public LoginResult login(LoginRequest loginRequest){
+        return null;
+    }
+    public LogoutResult register(LogoutRequest logoutRequest){
+        return null;
+    }
 
 }
